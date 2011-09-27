2250254840c88094d391267624d66150c93e039e
Himanshu <himanshu@Looney.(none)>
2011-09-27 18:01:08 -0500

    changed client

--- src/main/java/edu/ut/dsi/tickets/client/TicketClient.java
+++ src/main/java/edu/ut/dsi/tickets/client/TicketClient.java
@@ -6,34 +6,57 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import edu.ut.dsi.tickets.MethodRequest;
 import edu.ut.dsi.tickets.MethodResponse;
 
 public class TicketClient {
 
-  private int    port;
-  private String address;
-  private Socket socket;
+	private int    port;
+	private String address;
+	private Socket socket;
+	private int timeOut;
 
-  public TicketClient(String serverAddress, int port) {
-    this.address = serverAddress;
-    this.port = port;
-  }
+	public TicketClient(String serverAddress, int port, int timeOut) {
+		this.address = serverAddress;
+		this.port = port;
+		this.timeOut = timeOut;
+	}
 
-  private void connect() throws UnknownHostException, IOException {
-    if (socket == null) {
-      this.socket = new Socket();
-      this.socket.connect(new InetSocketAddress(this.address, this.port));
-    }
-  }
+	public TicketClient(String address, int port) {
+		this(address,port,0);
+	}
 
-  public MethodResponse send(MethodRequest request) throws IOException {
-    connect();
-    request.write(new DataOutputStream(socket.getOutputStream()));
-    MethodResponse response = new MethodResponse();
-    response.read(new DataInputStream(socket.getInputStream()));
-    return response;
-  }
+	private void connect() throws UnknownHostException, IOException {
+		if (socket == null) {
+			this.socket = new Socket();
+			this.socket.connect(new InetSocketAddress(this.address, this.port));
+		}
+	}
 
+	public MethodResponse send(MethodRequest request) throws IOException {
+		Timer timer = new Timer();
+		MethodResponse response = null;
+		timer.schedule(new EmptyTask() , timeOut*1000);
+		connect();
+		request.write(new DataOutputStream(socket.getOutputStream()));
+		response = new MethodResponse();
+		response.read(new DataInputStream(socket.getInputStream()));
+		timer.cancel();
+		return response;
+	}
+
+	public int getTimeOut() {
+		return timeOut;
+	}
+}
+class EmptyTask extends TimerTask{
+
+	@Override
+	public void run() {
+		// do nothing 
+	}
+	
 }
